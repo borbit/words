65092a4e1f8ef6911ad59a1cc2bb285756b883e6
Serge Borbit <serge.borbit@gmail.com>
2015-04-23 17:43:54 +0300

    wip

--- public/blocks/board/board.jsx.js
+++ public/blocks/board/board.jsx.js
@@ -5,7 +5,18 @@ var Letters = require('../letters/letters')
 module.exports = function() {
   return (
     <div className="board">
-      <div className="board__head"></div>
+      <div className="board__head">
+        <div className="btn-group">
+          <button className="btn btn-default" style={{width: 300, paddingTop: 4, paddingBottom: 4}} disabled>
+            <i className="fa fa-user"></i><br/>
+            <small>Роберт Дениро</small>
+          </button>
+          <button className="btn btn-default" style={{width: 301, paddingTop: 4, paddingBottom: 4}} disabled>
+            <i className="fa fa-user"></i><br/>
+            <small>Сильвестр Сталоне</small>
+          </button>
+        </div>
+      </div>
       <div className="board__tabel"><Field/></div>
       <div className="board__foot">
         <Letters/>
--- public/blocks/layout/layout.jsx.js
+++ public/blocks/layout/layout.jsx.js
@@ -8,41 +8,38 @@ var Me = require('../me/me')
 module.exports = function() {
   return (
     <div className="layout">
-      <hr className="hr hr_ver" style={{marginLeft: -112}}/>
-      <hr className="hr hr_ver" style={{marginLeft: -132}}/>
-      <hr className="hr hr_ver" style={{marginLeft: -488}}/>
-      <hr className="hr hr_ver" style={{marginLeft: 488}}/>
-
-      <hr className="hr hr_hor" style={{marginTop: -360}}/>
-      <hr className="hr hr_hor" style={{marginTop: -310}}/>
-      <hr className="hr hr_hor" style={{marginTop: -300}}/>
-      <hr className="hr hr_hor" style={{marginTop: 360}}/>
-
       <div className="layout__page">
         <div className="layout__content">
           <Board/>
         </div>
         <div className="layout__aside">
-          <div className="btn-group">
-            <button className="btn btn-default active" style={{width: 90}}>
-              <i className="fa fa-gamepad"></i><br/>
-              <small>Поточнi iгри</small>
-            </button>
-            <button className="btn btn-default" style={{width: 90}}>
-              <i className="fa fa-user"></i><br/>
-              <small>Профiль</small>
-            </button>
-            <button className="btn btn-default" style={{width: 90}}>
-              <i className="fa fa-users"></i><br/>
-              <small>Друзi</small>
-            </button>
-            <button className="btn btn-default" style={{width: 89}}>
-              <i className="fa fa-star"></i><br/>
-              <small>Лiдерборд</small>
-            </button>
+          <div className="panel panel-default">
+            <div className="panel-heading">
+              <div className="btn-group">
+                <button className="btn btn-default active" style={{paddingTop: 4, paddingBottom: 4}}>
+                  <i className="fa fa-gamepad"></i><br/>
+                  <small>Поточнi iгри</small>
+                </button>
+                <button className="btn btn-default" style={{paddingTop: 4, paddingBottom: 4}}>
+                  <i className="fa fa-user"></i><br/>
+                  <small>Профiль</small>
+                </button>
+                <button className="btn btn-default" style={{paddingTop: 4, paddingBottom: 4}}>
+                  <i className="fa fa-users"></i><br/>
+                  <small>Друзi</small>
+                </button>
+                <button className="btn btn-default" style={{paddingTop: 4, paddingBottom: 4}}>
+                  <i className="fa fa-star"></i><br/>
+                  <small>Лiдерборд</small>
+                </button>
+              </div>
+            </div>
           </div>
-          <Games/>
-          <Friends/>
+          <div className="panel-body">
+            asdasdasd
+            <Games/>
+            <Friends/>
+          </div>
         </div>
       </div>
     </div>
