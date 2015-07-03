6b7b27bbd84770c27f31423a855f2df6ab13e54c
Serge Borbit <serge.borbit@gmail.com>
2015-07-03 18:26:33 +0300

    wip

--- public/blocks/games/games__game.js
+++ public/blocks/games/games__game.js
@@ -1,9 +1,12 @@
 var React = require('react')
-var Waves = require('node-waves')
 var {PureRenderMixin} = React.addons
 var GameActions = require('../../js/actions/game')
 var GamesActions = require('../../js/actions/games')
 var render = require('./games__game.jsx')
+
+if (typeof(window) != 'undefined') {
+  var Waves = require('node-waves')
+}
 
 module.exports = React.createClass({
   mixins: [
